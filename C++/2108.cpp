437ef9ab5e74d38e94ae8f3cc0e960fd7f88be64
Sandor2889 <sandor2889@naver.com>
2022-05-18 21:06:36 +0900

    Update 2108.cpp

--- C++/2108.cpp
+++ C++/2108.cpp
@@ -1,44 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
 
-vector<int> vNums;
+int arr[8001] = { 0 };
 
 int main()
 {
+	int sum = 0;
+	int count = -9999;
+	int value = 0;
+	bool flag = false;
+
 	// 입력
 	int input;
 	cin >> input;
 
-	// 각 숫자들을 vector에 저장
+	// 각 수들을 vector에 저장
+	vector<float> vNums;
 	for (int idx = 0; idx < input; idx++)
 	{
 		int num;
 		cin >> num;
 		vNums.push_back(num);
+		sum += num;
+
+		arr[num + 4000]++;
 	}
 
-	// i와 j를 비교해가며 서로 자리를 바꿈
-	for (int i = 0; i < vNums.size(); i++)
+	// 오름차순 정렬
+	sort(vNums.begin(), vNums.end());
+
+	// 산술 평균
+	int avg = round(sum / (float)vNums.size());
+	cout << avg << '\n';
+
+	// 중앙 값
+	cout << vNums[vNums.size() / 2] << '\n';
+
+	// 최빈 값
+	for (int idx = 0; idx < 8001; idx++)
 	{
-		for (int j = i + 1; j < vNums.size(); j++)
+		if (arr[idx] == 0) { continue; }
+
+		// 가장많이 나온 수중에 두번째로 작은 값 찾기 (flag)
+		if (arr[idx] == count)
 		{
-			// 작은 수가 앞으로 가도록
-			if (vNums[i] > vNums[j])
+			if (flag)
 			{
-				int temp = vNums[i];
-				vNums[i] = vNums[j];
-				vNums[j] = temp;
+				value = idx - 4000;
+				flag = false;
 			}
 		}
-	}
+		if (arr[idx] > count)
+		{
+			count = arr[idx];
+			value = idx - 4000;
+			flag = true;
+		}
 
-	// 출력
-	for (int idx : vNums)
-	{
-		cout << idx << '\n';
 	}
+	cout << value << '\n';
+
+	// 범위 값
+	cout << vNums[vNums.size() - 1] - vNums[0];
 
 	return 0;
 }
