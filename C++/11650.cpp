b35a8565195489196697d367c222174af760e759
Sandor2889 <sandor2889@naver.com>
2022-05-19 21:27:45 +0900

    Create 11650.cpp

---
+++ C++/11650.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<pair<int, int>> vCoord;
+
+bool compare(pair<int, int> _a, pair<int, int> _b)
+{
+	// x 좌표가 같으면 y좌표를 비교
+	if (_a.first == _b.first)
+	{
+		return _a.second < _b.second;
+	}
+	else
+	{
+		return _a.first < _b.first;
+	}
+}
+
+int main()
+{
+	// 점의 수 입력
+	int nums;
+	cin >> nums;
+
+	for (int idx = 0; idx < nums; idx++)
+	{
+		pair<int, int> coord;
+		cin >> coord.first >> coord.second;
+		vCoord.push_back(coord);
+	}
+
+	// 오름차순 정렬
+	sort(vCoord.begin(), vCoord.end(), compare);
+
+	// 출력
+	for (int idx = 0; idx < vCoord.size(); idx++)
+	{
+		cout << vCoord[idx].first << ' ' << vCoord[idx].second << '\n';
+	}
+
+	return 0;
+}
