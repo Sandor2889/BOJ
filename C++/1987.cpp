3af59599063e26d77250d982582cef7ee0bc9f14
Sandor2889 <sandor2889@naver.com>
2022-07-11 00:11:37 +0900

    Update 1987.cpp

--- C++/1987.cpp
+++ C++/1987.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
-#include <vector>
 
 #define SIZE 21
+#define ALPHABETS 26
 
 using namespace std;
 
 char map[SIZE][SIZE];
-bool visited[SIZE][SIZE];
-vector<char> alphabet;
-vector<int> vCnt;
+int visited[ALPHABETS];
 
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, -1, 0, 1 };
 
 int r, c;
 string input;
-int cnt = 1;
-bool flag;
+int mexWeight = 0;
 
-
-// 동일 알파벳이 있는지 체크
-void checkAlphabet(int _nextX, int _nextY)
+void dfs(int _x, int _y, int _weight)
 {
-	for (int j = 0; j < alphabet.size(); j++)
-	{
-		if (alphabet[j] == map[_nextY][_nextX])
-		{
-			flag = true;
-		}
-	}
-}
+	visited[map[_y][_x] - 'A'] = true;
 
-void dfs(int _x, int _y)
-{
-	visited[_y][_x] = true;
-	alphabet.push_back(map[_y][_x]);
-	cnt++;
+	// 최대값 갱신
+	mexWeight = max(mexWeight, _weight);
 
+	// 다음 노드 확인
 	for (int i = 0; i < 4; i++)
 	{
 		int nextX = _x + dx[i];
@@ -44,17 +30,13 @@ void dfs(int _x, int _y)
 
 		if (0 <= nextX && nextX < c && 0 <= nextY && nextY < r)
 		{
-			checkAlphabet(nextX, nextY);
-			if (!visited[nextY][nextX] && !flag)
+			if (!visited[map[nextY][nextX] - 'A'])
 			{
-				flag = true;
-				dfs(nextX, nextY);
+				dfs(nextX, nextY, _weight + 1);
+				visited[map[nextY][nextX] - 'A'] = false;
 			}
 		}
 	}
-
-	vCnt.push_back(cnt);
-	cnt = 1;
 }
 
 int main()
@@ -62,14 +44,17 @@ int main()
 	// 세로 x 가로 : r x c
 	cin >> r >> c;
 
+	// 맵에 알파벳 입렵
 	for (int i = 0; i < r; i++)
 	{
 		cin >> map[i];
 	}
 
-	dfs(0, 0);
+	// dfs 실행
+	dfs(0, 0, 1);
 
-	cout << cnt;
+	// 최댓값 출력
+	cout << mexWeight;
 
 	return 0;
 }
