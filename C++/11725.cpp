0622cac57ec8f310d9b4be535639c8a9f3e49fc3
Sandor2889 <sandor2889@naver.com>
2022-07-14 23:20:48 +0900

    Create 11725.cpp

---
+++ C++/11725.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 100001
+
+using namespace std;
+
+vector<int> tree;
+int node;
+
+
+int main()
+{
+	// 트리 노드의 수 입력
+	cin >> node;
+
+	// 트리 노드 연결
+	for (int i = 0; i < node; i++)
+	{
+		int num1, num2;
+		cin >> num1 >> num2;
+
+	}
+
+
+	// dfs 실행
+
+
+	// 출력
+
+	return 0;
+}
