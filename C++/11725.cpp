0076c728ffc3b1df9c5f59ec3536df7905499449
Sandor2889 <sandor2889@naver.com>
2022-07-15 12:07:41 +0900

    Update 11725.cpp

--- C++/11725.cpp
+++ C++/11725.cpp
@@ -5,28 +5,49 @@
 
 using namespace std;
 
-vector<int> tree;
-int node;
+int arr[MAX];
+bool visited[MAX];
+vector<int> v[MAX];
 
+int n;
+
+void dfs(int _root)
+{
+	visited[_root] = true;
+	for (int i = 0; i < v[_root].size(); i++)
+	{
+		int next = v[_root][i];
+		if (!visited[next])
+		{
+			arr[next] = _root;
+			dfs(next);
+		}
+	}
+}
 
 int main()
 {
 	// 트리 노드의 수 입력
-	cin >> node;
+	cin >> n;
 
 	// 트리 노드 연결
-	for (int i = 0; i < node; i++)
+	for (int i = 0; i < n; i++)
 	{
 		int num1, num2;
 		cin >> num1 >> num2;
-
+		
+		v[num1].push_back(num2);
+		v[num2].push_back(num1);
 	}
 
-
 	// dfs 실행
-
+	dfs(1);
 
 	// 출력
+	for (int i = 2; i < n; i++)
+	{
+		cout << arr[i] << '\n';
+	}
 
 	return 0;
 }
