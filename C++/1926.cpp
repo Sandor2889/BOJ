f10d89dab2f6b0a0017c0b6ec0ab675c54c8f27e
Sandor2889 <sandor2889@naver.com>
2022-07-23 04:59:34 +0900

    Update 1926.cpp

--- C++/1926.cpp
+++ C++/1926.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <memory.h>
+#include <vector>
 
 #define MAX 501
 
 using namespace std;
 
 int n, m;
+int cnt;
+int maxWidth = 0;
 
 int map[MAX][MAX];
 bool visited[MAX][MAX];
+vector<int> v;
 
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, -1, 0, 1 };
@@ -16,15 +19,16 @@ int dy[] = { 0, -1, 0, 1 };
 void dfs(int _x, int _y)
 {
 	visited[_y][_x] = true;
+	cnt++;
 
 	for (int i = 0; i < 4; i++)
 	{
 		int nextX = _x + dx[i];
 		int nextY = _y + dy[i];
-		
+
 		if (0 <= nextX && nextX < m && 0 <= nextY && nextY < n)
 		{
-			if (!visited[nextY][nextX] && !map[nextY][nextX])
+			if (!visited[nextY][nextX] && map[nextY][nextX])
 			{
 				dfs(nextX, nextY);
 			}
@@ -51,12 +55,22 @@ int main()
 	{
 		for (int j = 0; j < m; j++)
 		{
-			if (!visited[i][j])
+			if (!visited[i][j] && map[i][j])
 			{
+				cnt = 0;
 				dfs(j, i);
+				v.push_back(cnt);
+
+				if (maxWidth <= cnt)
+				{
+					maxWidth = cnt;
+				}
 			}
 		}
 	}
+
+	// 출력 : 그림의 수, 가장 넓은 그림의 넓이
+	cout << v.size() << '\n' << maxWidth;
 
 	return 0;
 }
